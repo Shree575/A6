37db47db96127ac93403313e0157d989d368b940
Shree575 <55526642+Shree575@users.noreply.github.com>
2023-04-09 21:11:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB700 – Assignment 6
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name:Shreedhar Thapaliya Student ID:112749221 Date:2023-04-09
+Cyclic link:https://kind-tan-catfish-kit.cyclic.app/
+********************************************************************************/ 
+
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const exphbs = require("express-handlebars")
